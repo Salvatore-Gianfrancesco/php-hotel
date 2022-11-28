eff2998f385ecce0c81ffc7225fa3badb5667ffe
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-28 14:51:28 +0100

    added filters

--- index.php
+++ index.php
@@ -41,6 +41,18 @@ $hotels = [
     echo "$hotel[name]: $hotel[vote] stars.<br>Distance to center: $hotel[distance_to_center]Km<br><br>";
 } */
 
+if (isset($_GET['parking'])) {
+    $parking_filter = $_GET['parking'];
+} else {
+    $parking_filter = -1;
+}
+
+if (isset($_GET['vote'])) {
+    $vote_filter = $_GET['vote'];
+} else {
+    $vote_filter = -1;
+};
+
 ?>
 
 <!DOCTYPE html>
@@ -54,31 +66,65 @@ $hotels = [
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
 </head>
 
-<body class="bg-dark">
+<body class="bg-dark text-light">
+    <form action="index.php" method="GET">
+        <div class="d-flex justify-content-end align-items-center gap-4 m-3">
+            <!-- parking filter -->
+            <div>
+                <label for="parking">Parcheggio: </label>
+                <span>Si</span>
+                <input type="radio" name="parking" id="parking" value="1">
+                <span>No</span>
+                <input type="radio" name="parking" id="parking" value="0">
+            </div>
+
+            <!-- vote filter -->
+            <div>
+                <select class="form-select" name="vote" id="vote">
+                    <option disabled selected>Star rating</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+
+            <!-- submit & reset -->
+            <button type="submit" class="btn btn-light">Submit</button>
+            <button type="reset" class="btn btn-light">Reset</button>
+        </div>
+
+    </form>
+
     <div class="container">
         <h1 class="text-center my-5">Hotel list</h1>
+
+
         <div class="row row-cols-5">
             <?php foreach ($hotels as $hotel) : ?>
-                <div class="col">
-                    <div class="card bg-light pt-2 pb-5 px-2">
-                        <!-- hotel name -->
-                        <h3><?= $hotel['name'] ?></h3>
+                <?php if (($parking_filter === -1 || $hotel['parking'] == $parking_filter) && ($vote_filter === -1 || $hotel['vote'] == $vote_filter)) : ?>
+                    <div class="col">
+                        <div class="card bg-light text-dark pt-2 pb-5 px-2">
+                            <!-- hotel name -->
+                            <h3><?= $hotel['name'] ?></h3>
 
-                        <hr>
+                            <hr>
 
-                        <!-- hotel description -->
-                        <p><?= $hotel['description'] ?></p>
+                            <!-- hotel description -->
+                            <p><?= $hotel['description'] ?></p>
 
-                        <!-- hotel parking -->
-                        <div>Parcheggio: <?= ($hotel['parking'] ? 'SI' : 'NO') ?></div>
+                            <!-- hotel parking -->
+                            <div>Parcheggio: <?= ($hotel['parking'] ? 'SI' : 'NO') ?></div>
 
-                        <!-- hotel vote -->
-                        <div>Voto: <?= $hotel['vote'] . ' ' . ($hotel['vote'] === 1 ? 'stella' : 'stelle') ?></div>
+                            <!-- hotel vote -->
+                            <div>Voto: <?= $hotel['vote'] . ' ' . ($hotel['vote'] === 1 ? 'stella' : 'stelle') ?></div>
 
-                        <!-- hotel distance to center -->
-                        <div>Distanza dal centro: <?= $hotel['distance_to_center'] ?>Km</div>
+                            <!-- hotel distance to center -->
+                            <div>Distanza dal centro: <?= $hotel['distance_to_center'] ?>Km</div>
+                        </div>
                     </div>
-                </div>
+                <?php endif; ?>
             <?php endforeach; ?>
         </div>
     </div>
